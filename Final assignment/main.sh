9b5b9281de41f691ccb54cbefa1b7c005d88e349
Francesc Carandell Verdaguer <scur2184@int4.local.snellius.surf.nl>
2026-03-09 08:55:19 +0100

    Training pretrained v3

--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -4,8 +4,8 @@ wandb login
 python3 train.py \
     --data-dir ./data/cityscapes \
     --batch-size 32 \
-    --epochs 100 \
-    --lr 0.00005 \
+    --epochs 50 \
+    --lr 0.0005 \
     --num-workers 10 \
     --seed 42 \
-    --experiment-id "Pretrained_model-v2" \
+    --experiment-id "Pretrained_model-v3" \
--- Final assignment/model.py
+++ Final assignment/model.py
@@ -11,8 +11,8 @@ class Model(nn.Module):
             progress=True
         )
 
-        for param in self.model.parameters():
-            param.requires_grad = False
+       # for param in self.model.parameters():
+        #    param.requires_grad = False
 
         # Adjust the classifier head for 19 classes
         self.model.classifier[4] = nn.Conv2d(256, n_classes, kernel_size=1)
