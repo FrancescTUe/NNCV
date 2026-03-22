f64e83bc22200f56b8f04957615cfe76466b3271
Francesc <f.carandell.verdaguer@student.tue.nl>
2026-03-22 13:52:06 +0100

    bugs

--- Final assignment/model.py
+++ Final assignment/model.py
@@ -100,10 +100,10 @@ class FM_OODModel(nn.Module):
         # Global Average Pool both to 1D vectors
         v3 = torch.mean(feat_l3, dim=(2, 3)) # [Batch, 1024]
         v4 = torch.mean(feat_l4, dim=(2, 3)) # [Batch, 2048]
-        latent = torch.cat([v3, v4], dim=1), feat_l4
+        latent = torch.cat([v3, v4], dim=1)
 
         latent = F.normalize(latent, p=2, dim=1)
-        return latent
+        return latent, feat_l4
 
     def forward(self, x, return_ood_score=False):
         # we obtain the features and segmentation from baseline model
