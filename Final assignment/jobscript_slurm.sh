1f534a11985ba8318fe1607247dd26d5ab716870
Francesc <f.carandell.verdaguer@student.tue.nl>
2026-03-25 08:36:07 +0100

    Try w/o normalizer and higher lr

--- Final assignment/jobscript_slurm.sh
+++ Final assignment/jobscript_slurm.sh
@@ -6,6 +6,6 @@
 #SBATCH --partition=gpu_a100
 #SBATCH --time=12:00:00
 
-#srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
+srun apptainer exec --nv --env-file .env container.sif /bin/bash main.sh
 #srun apptainer exec --nv --env-file .env container.sif /bin/bash main_entropy.sh
-srun apptainer exec --nv --env-file .env container.sif /bin/bash main_latents.sh
+#srun apptainer exec --nv --env-file .env container.sif /bin/bash main_latents.sh
--- Final assignment/main.sh
+++ Final assignment/main.sh
@@ -6,7 +6,7 @@ python3 train.py \
     --ood-data-dir ./coco \
     --batch-size 128 \
     --epochs 60 \
-    --lr 0.0002 \
+    --lr 0.001 \
     --num-workers 10 \
     --seed 42 \
     --experiment-id "OOD_v4_DUMMY" \
